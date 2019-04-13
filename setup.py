726a4beae65cd7ab76e4b7e80cbde9423feca703
Trevor Xander <trevorcolexander@gmail.com>
2019-04-13 02:59:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,12 @@ def read(file_name):
     return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 setup(
-    name='Multithread Scraper',
-    version='0.1',
+    name='App Classifier',
+    version='0.2',
     url='https://github.com/trevorxander/AppClassifier',
     license='',
     author='Trevor Xander',
     author_email='trevorcolexander@gmail.com',
-    long_description=read('README'),
     install_requires=['numpy', 'pandas', 'scikit-learn', 'polyglot', 'PyICU', 'pycld2'],
     packages=find_packages()
-
 )
